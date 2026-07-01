3fb459d89579129ce15a392ef57936510145f8e7
gvaldez <gvaldez@justucuman.gov.ar>
2026-07-01 18:17:58 -0300

    modifying action update request

--- src/main/java/ar/org/proyungas/infrastructure/input/action/update/ActionUpdateRequest.java
+++ src/main/java/ar/org/proyungas/infrastructure/input/action/update/ActionUpdateRequest.java
@@ -1,5 +1,23 @@
 package ar.org.proyungas.infrastructure.input.action.update;
 
-public class ActionUpdateRequest {
+import com.fasterxml.jackson.annotation.JsonProperty;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Builder
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class ActionUpdateRequest {
+	@JsonProperty("applicant-id")
+    String applicantId;
+	@JsonProperty("uploaded-by-id")
+    String uploadedById;
+	@JsonProperty("derivative-status")
+    String derivativeStatus;
 }
